cf253f18465e09cb49598541db772a32fde7efe5
Vincent Anjiri <vincentanjiri12@gmail.com>
2024-10-14 20:17:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -510,16 +510,16 @@ print(f"Balance after {time} year/s: {total:.2f}")   """
 # print(dir(fruits))
 # print(help(fruits))
 # print(len(fruits))
-# print("pinnaple" in fruits)
+# print("pineapple" in fruits)
 #
-# fruits.add("pinnaple")
+# fruits.add("pineapple")
 # fruits.remove("apple")
 # fruits.pop()
 # fruits = ("apple", "banana", "orange", "coconut")
 # print(dir(fruits))
 # print(help(fruits))
 # print(len(fruits))
-# print("pinnaple" in fruits)
+# print("pineapple" in fruits)
 # print(fruits.index("apple"))
 # print(fruits.count("apple"))
 #
@@ -575,55 +575,251 @@ print(f"Balance after {time} year/s: {total:.2f}")   """
 # Python quiz game
 
 
-questions = (
-    "How man elements are in the periodic table: ",
-    "Which animal lays the largest eggs?: ",
-    "What is the most abundant gas in Earth's atmosphere?: ",
-    "How many bones are in the human body?: ",
-    "Which planet in the solar system is the hottest?: ")
+# questions = (
+#     "How man elements are in the periodic table: ",
+#     "Which animal lays the largest eggs?: ",
+#     "What is the most abundant gas in Earth's atmosphere?: ",
+#     "How many bones are in the human body?: ",
+#     "Which planet in the solar system is the hottest?: ")
+#
+# options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+#            ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+#            ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
+#            ("A. 206", "B. 207", "C. 208", "D. 209"),
+#            ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
 
-options = (("A. 116", "B. 117", "C. 118", "D. 119"),
-           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
-           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
-           ("A. 206", "B. 207", "C. 208", "D. 209"),
-           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
+# answers = ("C", "D", "A", "A", "B")
+# guesses = []
+# score = 0
+#
+# question_num = 0
+#
+# for question in questions:
+#     print("-------------------------")
+#     print(question)
+#     for option in options[question_num]:
+#         print(option)
+#     guess = input("Enter (A, B, C, D)").upper()
+#     guesses.append(guess)
+#     if guess == answers[question_num]:
+#         score += 1
+#         print("Correct")
+#     else:
+#         print("Incorrect")
+#         print(f"{answers[question_num ]} is the correct answer")
+#     question_num += 1
+#
+#
+# print("-------------------------")
+# print("          RESULTS        ")
+# print("-------------------------")
+#
+# print("answers", end=" ")
+# for answer in answers:
+#     print(answer, end=" ")
+#
+# print()
+#
+# for guess in guesses:
+#     print(guess, end=" ")
+#
+# print()
+#
+# score = int(score / len(questions) * 100)
+# print(f"Your score is {score}%")
 
-answers = ("C", "D", "A", "A", "B")
-guesses = []
-score = 0
 
-question_num = 0
+# dictionary = a collection of {key:value}pairs
+#              ordered and changeable. No duplicates
 
-for question in questions:
-    print("-------------------------")
-    print(question)
-    for option in options[question_num]:
-        print(option)
-    guess = input("Enter (A, B, C, D)").upper()
-    guesses.append(guess)
-    if guess == answers[question_num]:
-        score += 1
-        print("Correct")
-    else:
-        print("Incorrect")
-        print(f"{answers[question_num ]} is the correct answer")
-    question_num += 1
+# capitals = {"USA": "Washington D.C",
+#             "India": "New Delhi",
+#             "China": "Beijing",
+#             "Russia": "Moscow",
+#             "Kenya": "Nairobi"}
 
 
-print("-------------------------")
-print("          RESULTS        ")
-print("-------------------------")
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("Japan"))
 
-print("answers", end=" ")
-for answer in answers:
-    print(answer, end=" ")
 
-print()
+# if capitals.get("Kenya"):
+#     print("That capital exists")
+# else:
+#     print("That capital doesn't exist")
 
-for guess in guesses:
-    print(guess, end=" ")
+# capitals.update({"Germany": "Berlin"})
+# capitals.update({"USA": "Detroit"})
+# capitals.pop("China")
+# capitals.popitem()
+# capitals.clear()
 
-print()
+# keys = capitals.keys()
+# values = capitals.values()
+# for key in keys:
+#     print(key)
+#
+# print()
+#
+# for value in values:
+#     print(value)
 
-score = int(score / len(questions) * 100)
-print(f"Your score is {score}%")
+# items = capitals.items()
+# print(items)
+
+# for key, value in capitals.items():
+#     print(f"{key} ===== {value}")
+
+
+# concession stand program
+
+
+# dictionary {key:value}
+
+
+# menu = {"pizza": 3.00,
+#         "nachos": 4.50,
+#         "popcorn": 6.00,
+#         "fries": 2.50,
+#         "chips": 1.00,
+#         "pretzel": 3.50,
+#         "soda": 3.00,
+#         "lemonade": 4.25}
+#
+# cart = []
+# total = 0
+# print("-----------MENU--------------")
+# for key, value in menu.items():
+#     print(f"{key:10}: ${value:.2f}")
+# print("-----------------------------")
+
+# while True:
+#     food = input("Select an ite (q to quit)").lower()
+#     if food == 'q':
+#         break
+#     elif menu.get(food) is not None:
+#         cart.append(food)
+
+# print(cart)
+
+# print("------------ Your Oder ----------")
+# for food in cart:
+#     total += menu.get(food)
+#     print(food, end=" ")
+#
+# print()
+# print(f"Total is: ${total:2f}")
+
+
+# import random
+#
+# low = 1
+# high = 100
+#
+# options = ("rock", "paper", "scissors")
+# cards = ["2","3","4","5","6","7","8","9","10","J", "K", "Q", "A"]
+
+# number = random.randint(low,high)
+# number = random.random()
+# option = random.choice(options)
+# card = random.choice(cards)
+# card = random.shuffle(cards)
+# # print(option)
+#
+# print(card)
+
+# Python number guessing game
+
+# import random
+#
+# lowest_num = 1
+# highest_num = 100
+#
+# answer = random.randint(lowest_num, highest_num)
+#
+# guesses = 0
+# is_running = True
+#
+#
+# print("Python Number Guessing Game")
+# print(f"Select a number between {lowest_num} and {highest_num}")
+#
+#
+# while is_running:
+#     guess = input("Enter your guess: ")
+#
+#     if guess.isdigit():
+#         guess = int(guess)
+#         guesses += 1
+#
+#         if guess < lowest_num or guess > highest_num:
+#             print("That number is out of range")
+#             print(f"Please select a number between {lowest_num} and {highest_num}")
+#         elif guess < answer:
+#             print("Too low! Try again")
+#         elif guess > answer:
+#             print("Too high")
+#         else:
+#             print(f"CORRECT! The correct answer was {answer}")
+#             print(f"Number of guesses: {guesses}")
+#             is_running = False
+#     else:
+#         print("Invalid guess")
+#         print(f"Please select a number between {lowest_num} and {highest_num}")
+
+
+import random
+
+options = ("rock", "paper", "scissors")
+
+running =True
+
+while running:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors)")
+
+        print(f"Player: {player}")
+        print(f"Computer: {computer}")
+
+
+        if player == computer:
+            print("Its a tie")
+        elif player == 'rock' and computer == 'scissors':
+            print("You Win!")
+        elif player == "paper" and computer == 'rock':
+            print("You win")
+        elif player == 'scissor' and computer == "paper":
+            print("You win")
+        else:
+            print("You lose!")
+
+        if not input("Play again? (Y/N)").lower() == "y":
+            running = False
+
+print("Thanks for playing")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
